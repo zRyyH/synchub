4ab156bff8df0f691dc7cbb3523de9a9358a10f1
zRyyH <zryyh.br@gmail.com>
2025-12-07 13:02:28 -0300

    att

--- config/next.js
+++ config/next.js
@@ -1,2 +1,2 @@
-const NEXT_PUBLIC_URL = process.env.NODE_ENV != "production" ? "http://localhost:3000" : process.env.NEXT_PUBLIC_URL;
+const NEXT_PUBLIC_URL = process.env.NODE_ENV !== "production" ? "http://localhost:3000" : process.env.NEXT_PUBLIC_URL;
 export { NEXT_PUBLIC_URL };
